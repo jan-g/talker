4dff9d0f7318e5cabd5259819b425550e22b5c81
jan grant <jan.grant@oracle.com>
2018-01-11 15:02:10 +0000

    Add some line-based talker features
    The program's functionality is divided into three pieces: there's a
    Client, which handles the lifecycle of an individual connection; there's
    a Server, which joins them together and lets them find out about each
    other, and there are individual commands, which offer additional
    capabilities to the clients.

---
+++ talker/base.py
@@ -0,0 +1,157 @@
+import logging
+import select
+import socket
+
+
+LOG = logging.getLogger(__name__)
+
+
+class Socket:
+    def __init__(self, server=None, socket=None):
+        super().__init__()
+        self._server = server
+        self._socket = socket
+
+    @property
+    def server(self):
+        return self._server
+
+    @property
+    def socket(self):
+        return self._socket
+
+    def fileno(self):
+        return self.socket.fileno()
+
+    def has_output(self):
+        return False
+
+    def read(self):
+        """If the socket has something to read, handle that read and propagate events"""
+        pass
+
+    def write(self):
+        pass
+
+
+class ServerSocket(Socket):
+    def __init__(self, client_factory=None, **kwargs):
+        super().__init__(**kwargs)
+        self.client_factory = client_factory
+
+    def read(self):
+        client, addr = self.socket.accept()
+        self.server.add_client(self.client_factory(server=self.server, socket=client, addr=addr))
+
+
+class ClientSocket(Socket):
+    """You should override this, in particular these methods:
+    handle_new
+    handle_input
+    handle_close
+    """
+    def __init__(self, addr=None, **kwargs):
+        super().__init__(**kwargs)
+        self.addr = addr
+        self.in_buffer = bytes()
+        self.out_buffer = bytes()
+
+    def has_output(self):
+        return len(self.out_buffer) > 0
+
+    def read(self):
+        input = self.socket.recv(16384)
+        if len(input) == 0:
+            # Peer shutdown
+            self.close()
+        else:
+            self.in_buffer += input
+            self.handle_input()
+
+    def close(self):
+        self.socket.close()
+        self.server.remove_client(self)
+
+    def handle_close(self):
+        LOG.debug("Client is closed: %s", self.addr)
+
+    def handle_input(self):
+        LOG.debug("Received some bytes from the client: %s", self.in_buffer.decode("utf-8"))
+        self.in_buffer = bytes()
+
+    def write(self):
+        if len(self.out_buffer) > 0:
+            outlen = self.socket.send(self.out_buffer)
+            self.out_buffer = self.out_buffer[outlen:]
+
+    def queue_output(self, output):
+        self.out_buffer += output
+
+    def handle_new(self):
+        LOG.debug("New client: %s", self.addr)
+
+
+class Server(object):
+    """You may want to override this
+
+    In particular:
+    extend __init__
+    add additional methods used by the client factory
+    """
+    def __init__(self, host='0.0.0.0', port=8889, client_factory=ClientSocket):
+        self.clients = {ServerSocket(server=self, socket=self.make_socket(host, port), client_factory=client_factory)}
+
+    def make_socket(self, host, port):
+        s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen(5)
+        return s
+
+    def loop(self):
+        while len(self.clients) > 0:
+            readers = set(self.clients)
+            writers = {c for c in self.clients if c.has_output()}
+            readable, writable, x = select.select(readers, writers, [])
+            for r in readable:
+                r.read()
+
+            for w in writable:
+                w.write()
+
+    def add_client(self, client):
+        self.clients.add(client)
+        client.handle_new()
+
+    def remove_client(self, client):
+        self.clients.remove(client)
+        client.handle_close()
+
+
+class LineBuffered(ClientSocket):
+    """Extend the ClientSocket to add newline-delimited handling.
+
+    At this juncture, the APi turns from using sets of bytes to using strings."""
+
+    def handle_input(self):
+        while b'\r\n' in self.in_buffer:
+            line, found, self.in_buffer = self.in_buffer.partition(b'\r\n')
+
+            # Convert bytes to a string.
+            try:
+                s = line.decode('utf-8')
+                self.handle_line(s)
+            except UnicodeDecodeError:
+                LOG.warning("Client %s send non-utf-8 sequence", self.addr)
+
+    def output_line(self, line, eol=b'\r\n'):
+        """Enqueue a ilne of output to the client"""
+        self.queue_output(bytes(line, 'utf-8') + eol)
+
+    def handle_new(self):
+        super().handle_new()
+        self.output_line("Welcome, {}".format(self.addr))
+
+    def handle_line(self, line):
+        """Handle a line of input. It'll be in string form"""
+        LOG.debug("Received line of input from client %s: %s", self.addr, line)
--- talker/server.py
+++ talker/server.py
@@ -1,110 +1,65 @@
 import logging
-import select
-import socket
-
+import talker.base
 
 LOG = logging.getLogger(__name__)
 
 
-class Socket(object):
-    def has_output(self):
-        return False
+class Client(talker.base.LineBuffered):
 
-    def read(self, server, socket):
-        pass
+    def handle_new(self):
+        LOG.debug("New connection from %s", self.addr)
+        self.output_line("Welcome, {}".format(self.addr))
+        self.server.register_speaker(self)
+        self.server.tell_speakers("{} has joined".format(self.addr))
 
-    def write(self, server, socket):
-        pass
+    def handle_close(self):
+        self.server.unregister_speaker(self)
+        self.server.tell_speakers("{} has left".format(self.addr))
 
+    def handle_line(self, line):
+        """Handle a line of input. It'll be in string form"""
+        LOG.debug("Received line of input from client %s: %s", self.addr, line)
 
-class ServerSocket(Socket):
-    def __init__(self, client_factory):
-        self.client_factory = client_factory
+        # handle /-commands
+        if line.startswith("/"):
 
-    def read(self, server, socket):
-        client, addr = socket.accept()
-        server.add_client(client, self.client_factory(addr))
+            args = line.split()
+            if args[0] in COMMANDS:
+                COMMANDS[args[0]](self, args)
+            else:
+                self.output_line("Unknown command: {}".format(args[0]))
 
-
-class ClientSocket(Socket):
-    """You should override this, in particular these methods:
-    handle_new
-    handle_input
-    handle_close
-    """
-    def __init__(self, addr):
-        self.addr = addr
-        self.in_buffer = bytes()
-        self.out_buffer = bytes()
-
-    def has_output(self):
-        return len(self.out_buffer) > 0
-
-    def read(self, server, socket):
-        input = socket.recv(16384)
-        if len(input) == 0:
-            # Peer shutdown
-            self.handle_close(server, socket)
         else:
-            self.in_buffer += input
-            self.handle_input(server, socket)
 
-    def handle_close(self, server, socket):
-        LOG.debug("Client is closed: %s", self.addr)
-        socket.close()
-        server.remove_client(socket, self)
-
-    def handle_input(self, server, socket):
-        LOG.debug("Received some bytes from the client: %s", self.in_buffer.decode("utf-8"))
-        self.in_buffer = bytes()
-
-    def write(self, server, socket):
-        if len(self.out_buffer) > 0:
-            outlen = socket.send(self.out_buffer)
-            self.out_buffer = self.out_buffer[outlen:]
-
-    def queue_output(self, output):
-        self.out_buffer += output
-
-    def handle_new(self, server, socket):
-        LOG.debug("New client: %s", self.addr)
-        self.queue_output(bytes('Welcome, {}\r\n'.format(self.addr), 'utf-8'))
+            # By default, it's just a line of text
+            self.server.tell_speakers("{}: {}".format(self.addr, line))
 
 
-class Server(object):
-    """You may want to override this
+class Server(talker.base.Server):
+    def __init__(self, client_factory=Client, **kwargs):
+        super().__init__(client_factory=client_factory, **kwargs)
+        self.speakers = set()
 
-    In particular:
-    extend __init__
-    add additional methods used by the client factory
-    """
-    def __init__(self, host='0.0.0.0', port=8889, client_factory=ClientSocket):
-        self.clients = {self.make_socket(host, port): ServerSocket(client_factory)}
+    def register_speaker(self, client):
+        self.speakers.add(client)
 
-    def make_socket(self, host, port):
-        s = socket.socket()
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((host, port))
-        s.listen(5)
-        return s
+    def unregister_speaker(self, client):
+        self.speakers.remove(client)
 
-    def loop(self):
-        while len(self.clients) > 0:
-            readers = set(self.clients)
-            writers = {c for c in self.clients if self.clients[c].has_output()}
-            readable, writable, x = select.select(readers, writers, [])
-            for r in readable:
-                self.clients[r].read(self, r)
+    def tell_speakers(self, message, include=None, exclude=set()):
+        if include is None:
+            include = self.speakers
 
-            for w in writable:
-                self.clients[w].write(self, w)
+        for target in include:
+            if target not in exclude:
+                target.output_line(message)
 
-    def add_client(self, socket, client):
-        self.clients[socket] = client
-        client.handle_new(self, socket)
 
-    def remove_client(self, socket, client):
-        del self.clients[socket]
+# Commands
+def quit(client, args):
+    client.close()
 
-    def accept(self, socket):
-        return None
+
+COMMANDS = {
+    "/quit": quit,
+}
